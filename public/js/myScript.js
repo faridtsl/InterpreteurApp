30c03bc3e5e2e935f4db53e2509bb247156b2961
root <root@localhost.localdomain>
2016-07-28 20:07:00 +0100

    Modification Interpreteur V2 Corrigé

--- public/js/myScript.js
+++ public/js/myScript.js
@@ -1,54 +1,6 @@
 
 $(function () {
 
-    function ajaxAdr() {
-        // Get the record's ID via attribute
-        $id = $("#adr").val();
-
-        $.ajax({
-            url: '/adresse/'+$id,
-            type:"GET",
-            success:function(data){
-                $("#adresse").val(data['adresse']);
-                $("#pays").val(data['pays']);
-                $("#ville").val(data['ville']);
-                $("#departement").val(data['departement']);
-                $("#code_postal").val(data['code_postal']);
-                $("#numero").val(data['numero']);
-                $("#route").val(data['route']);
-                $("#lat").val(data['lat']);
-                $("#long").val(data['long']);
-            },error:function(){
-                alert("error!!!!");
-            }
-        });
-    }
-
-    function ajaxSubmit() {
-        $.ajax({
-            type: "POST",
-            url: '/interpreteur/update',
-            data: $("#updateForm").serialize(), // serializes the form's elements.
-            success: function(data){
-                location.reload();
-                alert(data); // show response from the php script.
-            },error:function (data) {
-                console.log(data);
-            }
-        });
-    }
-
-    $('#edit').modalSteps({
-        callbacks: {
-            '2': ajaxAdr
-        },
-        btnCancelHtml: 'Quitter',
-        btnPreviousHtml: 'Precedent',
-        btnNextHtml: 'Suivant',
-        btnLastStepHtml: 'Modifier',
-        completeCallback: ajaxSubmit
-    });
-
     $('#example').DataTable();
 
     $('.toggle').click(function (event) {
